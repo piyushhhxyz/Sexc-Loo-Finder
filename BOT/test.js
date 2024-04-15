0898e0505f76ef730451eec6fdc9ee68a62fd6ef
piyushhhxyz <piyush.bhawsar1@gmail.com>
2024-04-15 12:05:54 +0530

    troubleshooting EC2

--- BOT/test.js
+++ BOT/test.js
@@ -1,27 +1,41 @@
-const { Client, MessageMedia, LocalAuth, Location} = require('whatsapp-web.js');
+const { Client, MessageMedia, LocalAuth, Location } = require('whatsapp-web.js');
+const puppeteer = require('puppeteer');
 
 const client = new Client({
     webVersionCache: {
-      type: "remote",
-      remotePath:
-        "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
+        type: "remote",
+        remotePath:
+            "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
     },
-    authStrategy: new LocalAuth(),
+    // authStrategy: new LocalAuth(),
+    puppeteer: {
+        args: [
+          '--no-sandbox',
+          '--disable-setuid-sandbox'
+        ],
+        authStrategy:  new LocalAuth(),
+    }
 });
 
-client.on('ready', () => {
+client.on('ready', async () => {
     console.log('WhatsApp Bot is ready!');
+    try {
+        const browser = await puppeteer.launch({
+            executablePath: '/usr/bin/chromium-browser'
+        });
+        // Add your Puppeteer code here, such as navigating to a webpage or taking a screenshot
+        await browser.close(); // Close the browser after finishing your Puppeteer tasks
+    } catch (error) {
+        console.error('Error launching Puppeteer:', error);
+    }
 });
 
 client.on('message', async (msg) => {
     const chat = await msg.getChat();
     if (chat) {
         chat.sendStateTyping();
-        // setTimeout(() => {
-            chat.sendMessage('I am a bot');
-        // }, 2000);
+        chat.sendMessage('I am a bot');
     }
 });
 
 client.initialize();
-
